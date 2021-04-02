4c26f8519ab07398c42ede49a4328098fad7ceb0
teru <teru@1f8.dev>
2021-04-02 17:19:42 +0900

    added convertIntTz

--- src/index.ts
+++ src/index.ts
@@ -34,9 +34,59 @@ export const intToTime = (n: number): Date => {
 }
 
 export const convertDateTz = (dt: Date, tz: TimeZone): Date => {
-  return new Date()
+
+  if (tz === TimeZone.Japan) {
+    let utcTime = dt.getTime() + 32400000;
+    return new Date(utcTime)
+  } else if (tz === TimeZone.UsHawaii) {
+    let utcTime = dt.getTime() - 36000000;
+    return new Date(utcTime)
+  } else if (tz === TimeZone.AustrlianEastern) {
+    let utcTime = dt.getTime() + 39600000;
+    return new Date(utcTime)
+  } else if (tz === TimeZone.AustrlianCentral) {
+    let utcTime = dt.getTime() + 34200000;
+    return new Date(utcTime)
+  } else if (tz === TimeZone.AustrlianWestern) {
+    let utcTime = dt.getTime() + 28800000;
+    return new Date(utcTime)
+  } else if (tz === TimeZone.UsPacific) {
+    let utcTime = dt.getTime() - 28800000;
+    return new Date(utcTime)
+  } else if (tz === TimeZone.UsEastern) {
+    let utcTime = dt.getTime() - 18000000;
+    return new Date(utcTime)
+  } else {
+    throw Error("Not supported TimeZone");
+  }
 }
 
-export const convertIntTz = (n: Number, h: Number, tz: TimeZone): Date => {
-  return new Date()
+export const convertIntTz = (n: number, h: number, tz: TimeZone): Date => {
+  const utc = intToDate(n).getTime()
+  const hourInMil = h * 3600000;
+  const utcDateTime = utc + hourInMil;
+  if (tz === TimeZone.Japan) {
+    let utcTime = utcDateTime + 32400000;
+    return new Date(utcTime)
+  } else if (tz === TimeZone.UsHawaii) {
+    let utcTime = utcDateTime - 36000000;
+    return new Date(utcTime)
+  } else if (tz === TimeZone.AustrlianEastern) {
+    let utcTime = utcDateTime + 39600000;
+    return new Date(utcTime)
+  } else if (tz === TimeZone.AustrlianCentral) {
+    let utcTime = utcDateTime + 34200000;
+    return new Date(utcTime)
+  } else if (tz === TimeZone.AustrlianWestern) {
+    let utcTime = utcDateTime + 28800000;
+    return new Date(utcTime)
+  } else if (tz === TimeZone.UsPacific) {
+    let utcTime = utcDateTime - 28800000;
+    return new Date(utcTime)
+  } else if (tz === TimeZone.UsEastern) {
+    let utcTime = utcDateTime - 18000000;
+    return new Date(utcTime)
+  } else {
+    throw Error("Not supported TimeZone");
+  }
 }
