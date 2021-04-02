83e006d8ed7af9f380d21ffbce28e4390c9bb971
kwa <kwa@1f8.dev>
2021-04-02 17:41:18 +0900

    Refactor

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,26 @@ export enum TimeZone {
   Japan,
 }
 
+class TimeZoneInfo {
+  tz: TimeZone
+  offset: number
+  isoName: string
+  constructor(tz:TimeZone, offset:number, isoName:string) {
+    this.tz = tz
+    this.offset = offset
+    this.isoName = isoName
+  }
+}
+
+const timeZoneOffsets = {}
+timeZoneOffsets[TimeZone.Japan           ] = new TimeZoneInfo(TimeZone.Japan           ,  1000*60*60*9    , 'Tokyo');
+timeZoneOffsets[TimeZone.UsHawaii        ] = new TimeZoneInfo(TimeZone.UsHawaii        , -36000000        , ''     );
+timeZoneOffsets[TimeZone.AustrlianEastern] = new TimeZoneInfo(TimeZone.AustrlianEastern,  39600000        , ''     );
+timeZoneOffsets[TimeZone.AustrlianCentral] = new TimeZoneInfo(TimeZone.AustrlianCentral,  34200000        , ''     );
+timeZoneOffsets[TimeZone.AustrlianWestern] = new TimeZoneInfo(TimeZone.AustrlianWestern,  28800000        , ''     );
+timeZoneOffsets[TimeZone.UsPacific       ] = new TimeZoneInfo(TimeZone.UsPacific       , -28800000        , ''     );
+timeZoneOffsets[TimeZone.UsEastern       ] = new TimeZoneInfo(TimeZone.UsEastern       , -18000000        , ''     );
+
 export const dateToInt = (dt: Date): number => {
   const isoDate = dt.toISOString()
   const dateString = isoDate.split("T")
@@ -35,58 +55,17 @@ export const intToTime = (n: number): Date => {
 
 export const convertDateTz = (dt: Date, tz: TimeZone): Date => {
 
-  if (tz === TimeZone.Japan) {
-    let utcTime = dt.getTime() + 32400000;
-    return new Date(utcTime)
-  } else if (tz === TimeZone.UsHawaii) {
-    let utcTime = dt.getTime() - 36000000;
-    return new Date(utcTime)
-  } else if (tz === TimeZone.AustrlianEastern) {
-    let utcTime = dt.getTime() + 39600000;
-    return new Date(utcTime)
-  } else if (tz === TimeZone.AustrlianCentral) {
-    let utcTime = dt.getTime() + 34200000;
-    return new Date(utcTime)
-  } else if (tz === TimeZone.AustrlianWestern) {
-    let utcTime = dt.getTime() + 28800000;
-    return new Date(utcTime)
-  } else if (tz === TimeZone.UsPacific) {
-    let utcTime = dt.getTime() - 28800000;
-    return new Date(utcTime)
-  } else if (tz === TimeZone.UsEastern) {
-    let utcTime = dt.getTime() - 18000000;
-    return new Date(utcTime)
-  } else {
+  if (!timeZoneOffsets.hasOwnProperty(tz)) {
     throw Error("Not supported TimeZone");
   }
+
+  let utcTime = dt.getTime() + timeZoneOffsets[tz].offset;
+  return new Date(utcTime)
+
 }
 
 export const convertIntTz = (n: number, h: number, tz: TimeZone): Date => {
   const utc = intToDate(n).getTime()
   const hourInMil = h * 3600000;
-  const utcDateTime = utc + hourInMil;
-  if (tz === TimeZone.Japan) {
-    let utcTime = utcDateTime + 32400000;
-    return new Date(utcTime)
-  } else if (tz === TimeZone.UsHawaii) {
-    let utcTime = utcDateTime - 36000000;
-    return new Date(utcTime)
-  } else if (tz === TimeZone.AustrlianEastern) {
-    let utcTime = utcDateTime + 39600000;
-    return new Date(utcTime)
-  } else if (tz === TimeZone.AustrlianCentral) {
-    let utcTime = utcDateTime + 34200000;
-    return new Date(utcTime)
-  } else if (tz === TimeZone.AustrlianWestern) {
-    let utcTime = utcDateTime + 28800000;
-    return new Date(utcTime)
-  } else if (tz === TimeZone.UsPacific) {
-    let utcTime = utcDateTime - 28800000;
-    return new Date(utcTime)
-  } else if (tz === TimeZone.UsEastern) {
-    let utcTime = utcDateTime - 18000000;
-    return new Date(utcTime)
-  } else {
-    throw Error("Not supported TimeZone");
-  }
+  return convertDateTz(new Date(utc + hourInMil), tz)
 }
